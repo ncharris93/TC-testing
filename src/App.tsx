3ce5cbe5557d4cdcdcc328fe7c352e1134215d62
Noah <ncharris93@gmail.com>
2021-04-02 09:44:23 -0400

    add 2nd example, update App.tsx & tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+
 import { SimpleButton } from './exmple1/Button'
-import { Typography } from '@material-ui/core'
+import { makeStyles, Typography } from '@material-ui/core'
+import { SimpleButton2Parent } from './exmple2/Button2'
 
 function App() {
+  const classes = useStyles()
+
   return (
     <div className="App">
       <header className="App-header">
-        <Typography>Example 1</Typography>
-        <SimpleButton />
+        <div className={classes.exampleContainer}>
+          <Typography>Example 1</Typography>
+          <SimpleButton />
+        </div>
+        <div className={classes.exampleContainer}>
+          <Typography>Example 2</Typography>
+          <SimpleButton2Parent />
+        </div>
       </header>
     </div>
   )
 }
+
+const useStyles = makeStyles({
+  exampleContainer: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+})
 
 export default App
---
+++ src/exmple2/Button2.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+import { SimpleButton2 } from './Button2'
+
+describe('SimpleButton', () => {
+  it('should exist', () => {
+    const { getByRole } = render(
+      <SimpleButton2 onClick={jest.fn()} count={0} />
+    )
+    const component = getByRole('simple-button-2')
+    expect(component).toBeDefined()
+  })
+
+  it('should display the count prop', () => {
+    const { getByText } = render(
+      <SimpleButton2 onClick={jest.fn()} count={0} />
+    )
+    expect(getByText(/0/)).toBeInTheDocument()
+  })
+
+  it('should run the click function when the button is clicked', () => {
+    const mockClick = jest.fn()
+
+    const { getByRole } = render(
+      <SimpleButton2 onClick={mockClick} count={0} />
+    )
+    const btn = getByRole('simple-button-2')
+
+    fireEvent.click(btn)
+
+    expect(mockClick).toHaveBeenCalled()
+  })
+})
