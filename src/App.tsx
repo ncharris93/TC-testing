3f7a55e24f0b581bfbb41e30bf649af7bc6d7677
Noah <ncharris93@gmail.com>
2021-04-02 09:23:56 -0400

    add example 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { SimpleButton } from './exmple1/Button'
+import { Typography } from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography>Example 1</Typography>
+        <SimpleButton />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
